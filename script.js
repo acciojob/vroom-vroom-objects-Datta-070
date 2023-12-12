a06090b1d45fd6446717ad2b9b2305bcf3a451e2
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-12-12 14:32:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make=make;
+	this.model=model;
+}
+Car.prototype.getMakeModel(){
+	return `${this.make} ${this.model}`;
+}
+function sportsCar(make, model, topSpeed) {
+	Car.call(this, make, model);
+	this.topSpeed=topSpeed;
+}
+sportsCar.prototype.getTopSpeed=function(){
+	return this.topSpeed;
+}
+SportsCar.prototype.__proto__ = Car.prototype;
 
 function SportsCar(make, model, topSpeed) {}
 
